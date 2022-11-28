bc1247d9cef317e4e9b09be29ec84c1348435568
Valeri85 <valeriousmonk85@gmail.com>
2022-11-28 18:24:16 +0400

    add search section

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-//* Burger Menu
+// Header
+const header = document.getElementById('header');
+const main = document.getElementById('main');
 
-// Select navigation burger menu button element
-const burger = document.querySelector('.header__burger');
-// Select menu list element (ul)
-const menu = document.querySelector('.header__menu');
+const headerHeight = header.getBoundingClientRect().height;
+main.style.paddingTop = `${headerHeight}px`;
 
-/* 
-    Adding an event listener to the burger menu button. 
-    When the burger menu button is clicked, it will toggle the class 'opened' on itself. 
-    If the burger menu button has the class 'opened', it will set the attribute 'aria-expanded' to true. 
-    If the burger menu button does not have the class 'opened', it will set the attribute 'aria-expanded' to false. 
-    It will also toggle the class 'show' on the menu list. 
-*/
+/*------------------------------------------------------------------------------*/
+
+// Burger Menu
+const burger = header.querySelector('.header__burger');
+const menu = header.querySelector('.header__menu');
+
 burger.addEventListener('click', () => {
 	burger.classList.toggle('opened');
 
